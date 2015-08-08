7ffa7d7c7f48ce46a66d354663f902ba80d2affa
commonfire <zdbeijing66@126.com>
2015-08-09 00:37:22 +0800

    add Solution for FindKthSmallest&&Largest problem

--- src/dango_algorithmcode_class2/FindKthSmallest.java
+++ src/dango_algorithmcode_class2/FindKthSmallest.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
  * @author zjd
  * 找序列中第K大的数
  */
-public class FindKthMax {
+public class FindKthSmallest {
 	
-	private static int findKthMax(Integer[] a,int k,int low,int high){
+	private static int findKthSmallest(Integer[] a,int k,int low,int high){
 		int num = 0;
 		ArrayList<Integer> left = new ArrayList<Integer>();
 		ArrayList<Integer> right = new ArrayList<Integer>();	 
@@ -24,10 +24,10 @@ public class FindKthMax {
 		if(left.size() == k) return key;
 	
 		if(left.size()>k){
-			num = findKthMax(left_arr, k, ++low, left_arr.length-1);
+			num = findKthSmallest(left_arr, k, ++low, left_arr.length-1);
 		}
 		else{
-			num = findKthMax(right_arr, k-left_arr.length, 0, right_arr.length-1);
+			num = findKthSmallest(right_arr, k-left_arr.length, 0, right_arr.length-1);
 		}
 		return num;
 	}
@@ -44,15 +44,15 @@ public class FindKthMax {
 		return low;	
 	}
 	
-	private int findKthMax2(int[] a,int k,int low,int high){
+	private int findKthSmallest2(int[] a,int k,int low,int high){
 		int ans = 0;
 		if(low >= high) return a[low];
 		else{
 			int mid = sort(a, low, high);
-			if(mid > k-1)     //注意判断条件不是>k!!
-				ans = findKthMax2(a, k, low, mid-1);
+			if(mid > k-1)     //注意判断条件不是>k!!  注意第k大等价于n-k+1小，其中n为数组长度！！leetcode上有求第K大的数，用此方法
+				ans = findKthSmallest2(a, k, low, mid-1);
 			else if(mid < k-1) 
-				ans = findKthMax2(a, k, mid+1, high);
+				ans = findKthSmallest2(a, k, mid+1, high);
 			else 
 				ans = a[mid];
 		    return ans; 
@@ -62,11 +62,10 @@ public class FindKthMax {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Integer[] a = new Integer[]{1,4,3,2};
-//		System.out.println(findKthMax(a, 2, 0, a.length-1));
+//		Integer[] a = new Integer[]{6,2,4,1};
+//		System.out.println(findKthSmallest(a, 2, 0, a.length-1));
 		int[] a = new int[]{6,2,4,1};
-//		System.out.println(new FindKthMax().sort(a, 0, a.length-1));
-		System.out.println(new FindKthMax().findKthMax2(a, 3, 0, a.length-1));
+		System.out.println(new FindKthSmallest().findKthSmallest2(a, 1, 0, a.length-1));
 	}
 
 }
